6cc2e346e3dccd28fc5a2d998cddf7147b7c3c7d
Mironchikov Ilya <i.mironchikov@gmail.com>
2018-05-15 17:01:38 +0300

    commit

--- web/public/index.php
+++ web/public/index.php
@@ -1,4 +1,50 @@
 
 <?php
 include '../app/vendor/autoload.php';
-$g = 'test'; $c = function($a, $b) use($g){ echo $a . $b . $g; }; $g = 'test2'; var_dump($c);
+$g = 'test';
+$c = function($a, $b) use($g) {
+    echo $a . $b . $g;
+};
+$g = 'test2';
+$d = 'test3';
+$c(1, 2);
+var_dump($c);
+
+(function() use($g, $d) {
+    echo $g;
+    echo $d;
+})();
+
+abstract class A
+{
+    abstract function test(string $s);
+}
+
+echo '<hr/>';
+
+$arr = ["sitepoint", "phpmaster", "buildmobile", "rubysource",
+    "designfestival", "cloudspring"];
+
+// Создаём новый итератор ArrayIterator на основе массива
+$iter = new ArrayIterator($arr);
+// Итерируем
+foreach($iter as $key => $value) {
+    echo $key . ":  " . $value . "<br>";
+}
+var_dump($iter);
+echo '<hr/>';
+// Многомерный массив
+$arr = [
+    ["sitepoint", "phpmaster"],
+    ["buildmobile", "rubysource"],
+    ["designfestival", "cloudspring"],
+    "not an array"
+];
+
+$iter = new RecursiveArrayIterator($arr);
+
+// Итерируем объект.
+// Для этого нам понадобится экземпляр RecursiveIteratorIterator
+foreach(new RecursiveIteratorIterator($iter) as $key => $value) {
+    echo $key . ": " . $value . "<br>";
+}
